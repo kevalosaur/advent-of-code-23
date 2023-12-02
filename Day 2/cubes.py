3baa375b1de67efdefc9ba8afd727b42a9292f66
kevalosaur <kddchess@yahoo.com>
2023-12-02 03:25:37 -0600

    Day 2 complete!

---
+++ Day 2/cubes.py
@@ -0,0 +1,16 @@
+from re import match, search
+
+tot = 0
+i = 1
+for l in open('input.txt', 'r').readlines():
+    trimmed = l[l.index(':')+1:].replace(' ', '').split(';')
+    for t in trimmed:
+        mr, mg, mb = search(r'(\d+)red', t), search(r'(\d+)green', t), search(r'(\d+)blue', t)
+        r, g, b = (int(mr.group(1)) if mr else 0, int(mg.group(1)) if mg else 0, int(mb.group(1)) if mb else 0)
+        if r > 12 or g > 13 or b > 14:
+            break
+    else:
+        tot += i
+    i += 1
+
+print(tot)
---
+++ Day 2/cubes_p2.py
@@ -0,0 +1,13 @@
+from re import match, search
+
+tot = 0
+for l in open('input.txt', 'r').readlines():
+    trimmed = l[l.index(':')+1:].replace(' ', '').split(';')
+    minr, ming, minb = 0, 0, 0
+    for t in trimmed:
+        mr, mg, mb = search(r'(\d+)red', t), search(r'(\d+)green', t), search(r'(\d+)blue', t)
+        r, g, b = (int(mr.group(1)) if mr else 0, int(mg.group(1)) if mg else 0, int(mb.group(1)) if mb else 0)
+        minr, ming, minb = max(minr, r), max(ming, g), max(minb, b)
+    tot += minr*ming*minb
+
+print(tot)
